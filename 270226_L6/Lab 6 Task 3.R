48ace56d1bf90162101b1ca4e4c7c8c537238dab
adrianahafiy <adrianahafiyykopy@gmail.com>
2026-02-27 11:10:02 +0800

    270226_L6

---
+++ 270226_L6/Lab 6 Task 3.R
@@ -0,0 +1,22 @@
+#create table
+emp.data <- data.frame(
+  name = c("Anastasia", "Dima", "Michael", "Matthew", "Laura", "Kevin", "Jonas"),
+  score = c(12.5, 9.0, 16.5, 12.0, 9.0, 8.0, 19.0),
+  attempts = c(1, 3, 2, 3, 2, 1, 2)
+)
+print(emp.data)
+
+#add qualify column
+emp.data$qualify <- c("yes", "no", "yes", "no", "no", "no", "yes" )
+print(emp.data)
+
+#add data
+emp.newdata <- data.frame(
+  name = c("Emily"),
+  score = c(14.5),
+  attempts = c (1), 
+  qualify = c("yes")
+)
+
+emp.finaldata <- rbind(emp.data, emp.newdata)
+print(emp.finaldata)
